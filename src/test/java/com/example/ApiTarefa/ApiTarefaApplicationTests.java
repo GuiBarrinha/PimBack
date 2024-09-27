c808b6a57135d07bb7f3d25cfa198623b3b760e0
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-09-27 16:27:40 -0300

    Projeto criado

---
+++ src/test/java/com/example/ApiTarefa/ApiTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiTarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
