7180608aa9e1760c19de3bc553ebedbb2a1a1b5a
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-09-28 13:15:38 -0300

    Incluido repositorio de documentação: springdoc-openapi-starter-webmvc-ui e springdoc-openapi-starter-webmvc-api

--- src/main/java/com/example/ApiTarefa/ApiTarefaApplication.java
+++ src/main/java/com/example/ApiTarefa/ApiTarefaApplication.java
@@ -3,7 +3,20 @@ package com.example.ApiTarefa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Documentação API Tarefas",
+				version = "1.0",
+				description = "Documentação de uso dos endpoints API Tarefas",
+				contact = @Contact(name="Guilherme", email="guilhermebarrinhams@gmail.com", url="https://github.com/GuiBarrinha/estagioStefanini")
+				)		
+		)
+
 public class ApiTarefaApplication {
 
 	public static void main(String[] args) {
